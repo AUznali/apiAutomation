75fc6436e86b36edf5cbf9359fdba4b90c16d7ed
SVPA-AnriUznali <iposter@Anris-Mac-mini.internet.sony.tv>
2018-04-10 15:45:26 -0700

    Updated comparingJsonsData Tool

--- bin/test/jasmine/api/contentValidation/tools/compareJsons.js
+++ bin/test/jasmine/api/contentValidation/tools/compareJsons.js
@@ -14,16 +14,38 @@ async function comparingJsonsData() {
 
   //Gettings Arrays with Content objects from the Server and Admin JSONs
   var adminContentJson = await pullToArray(mainOptions, certainOptions.admin);
-  var serverContentJson = await pullToArray(mainOptions, certainOptions.admin);
+  var serverContentJson = await pullToArray(mainOptions, certainOptions.blueServer);
 
   //Taking number of the contnet files (Here it's number of objects in the array = array length)
   var lengthAdminContentJson = adminContentJson.length;
   var lengthServerContentJson = serverContentJson.length;
 
 
-  //  = = = = = TEST = = = = = Comparing number of the contnet files on the SERVER and on the ADMIN
-  assert.equal(lengthAdminContentJson, lengthServerContentJson, "NUMBER OF THE CONTENT FILES ARE NOT THE SAME: ADMIN JSON: " +
-    lengthAdminContentJson + " / SERVER JSON: " + lengthServerContentJson); //OK
+  //  = = = = = TEST 1 = = = = = Comparing number of the contnet files on the SERVER and on the ADMIN
+  // assert.equal(lengthAdminContentJson, lengthServerContentJson, "NUMBER OF THE CONTENT FILES ARE NOT THE SAME: ADMIN JSON: " +
+  //   lengthAdminContentJson + " / SERVER JSON: " + lengthServerContentJson);
+  // = = = = = = = = = = = = =  = = = = = = = = = = = = =  = = = = = = = = = = = = =  = = = = = = = = = = = = =  = = = = = = = = = = = = =  = = = = = = = = = = = =
+
+
+
+//  = = = = = TEST 2 = = = = = Working with two arrays. I take string title+type (type = language), than serch same in another array.
+// When find, do check for all other rows.
+  for (var i = 0; i < lengthAdminContentJson; i++) {
+    var titlePlusLangAdmin = adminContentJson[i].title + adminContentJson[i].type;
+
+    for (var j = 0; j < lengthServerContentJson; j++) {
+      var titlePlusLangServer = serverContentJson[j].title + serverContentJson[j].details.type;
+     if(titlePlusLangAdmin.localeCompare(titlePlusLangServer)){
+       console.log(titlePlusLangAdmin + " = " + titlePlusLangServer);
+       // Here I do compare between same contnet: ADMIN /SERVER;
+       
+      }
+    }
+
+
+  }
 };
+
+
 
 comparingJsonsData();
